c2fb6a34c7445dd64a04b76dd73a9e547dc1bb1e
Beatrice BIGEL <bebigel@made-f0Br1s6.clusters.42paris.fr>
2024-07-22 17:04:13 +0200

    load csv in postgres

---
+++ postgres_db/setup.sql
@@ -0,0 +1,44 @@
+-- setup.sql
+
+-- Create tables
+CREATE TABLE data_2022_oct (
+    event_time TIMESTAMP,
+    event_type VARCHAR(100),
+    product_id INTEGER,
+    price NUMERIC(10, 2),
+    user_id BIGINT,
+    user_session UUID
+);
+
+CREATE TABLE data_2022_nov (
+    event_time TIMESTAMP,
+    event_type VARCHAR(100),
+    product_id INTEGER,
+    price NUMERIC(10, 2),
+    user_id BIGINT,
+    user_session UUID
+);
+
+CREATE TABLE data_2022_dec (
+    event_time TIMESTAMP,
+    event_type VARCHAR(100),
+    product_id INTEGER,
+    price NUMERIC(10, 2),
+    user_id BIGINT,
+    user_session UUID
+);
+
+CREATE TABLE data_2023_jan (
+    event_time TIMESTAMP,
+    event_type VARCHAR(100),
+    product_id INTEGER,
+    price NUMERIC(10, 2),
+    user_id BIGINT,
+    user_session UUID
+);
+
+-- Import data into tables
+COPY data_2022_oct FROM '/docker-entrypoint-initdb.d/data_2022_oct.csv' DELIMITER ',' CSV HEADER;
+COPY data_2022_nov FROM '/docker-entrypoint-initdb.d/data_2022_nov.csv' DELIMITER ',' CSV HEADER;
+COPY data_2022_dec FROM '/docker-entrypoint-initdb.d/data_2022_dec.csv' DELIMITER ',' CSV HEADER;
+COPY data_2023_jan FROM '/docker-entrypoint-initdb.d/data_2023_jan.csv' DELIMITER ',' CSV HEADER;
