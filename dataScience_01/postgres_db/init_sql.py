2be7951e17ce05d5ab7c148f18a4e9d082f36070
Beatrice BIGEL <bebigel@made-f0Br5s12.clusters.42paris.fr>
2024-09-04 17:15:12 +0200

    DS 01

--- dataScience_01/postgres_db/init_sql.py
+++ dataScience_01/postgres_db/init_sql.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 docker_path = '/docker-entrypoint-initdb.d/'
 data_path = '/data/'
-# data_path = './customer/'        # for test purpose
+# data_path = './customer/'        for test purpose
 
 column_types = {
     'event_time': 'TIMESTAMP WITH TIME ZONE',
@@ -48,12 +48,12 @@ def generate_sql(f):
         columns = next(csv.reader(file))
 
     print(f'Processing {f}..., columns: {columns}')
-    if columns[0] != 'event_time':
-        raise ValueError(f"First column of {f} is not 'event_time'")
+    # if columns[0] != 'event_time':
+    #     raise ValueError(f"First column of {f} is not 'event_time'")
 
     print(f'Checking if event_time is in DATETIME format...')
-    if not is_datetime_column(file_path):
-        raise ValueError(f"Column 'event_time' of {f} is not in DATETIME format")
+    # if not is_datetime_column(file_path):
+    #     raise ValueError(f"Column 'event_time' of {f} is not in DATETIME format")
 
     sql_col = ',\n\t'.join([f'{col} {column_types.get(col, "VARCHAR")}' for col in columns])
     return f'CREATE TABLE {f[:-4]} (\n\t{sql_col}\n);\n'
@@ -69,7 +69,7 @@ for f in csv_files:
     except ValueError as e:
         print(e)
 
-sql_content += 'CREATE TABLE customers AS (\n'
+sql_content += 'CREATE TABLE customers_all AS (\n'
 
 size = len(csv_files)
 for f in csv_files:
@@ -78,6 +78,18 @@ for f in csv_files:
         sql_content += 'UNION ALL\n'
     size -= 1
 sql_content += ');\n'
+
+sql_content += 'CREATE TABLE customers AS (\n'
+
+size = len(csv_files)
+for f in csv_files:
+    sql_content += f'SELECT * FROM {f[:-4]}\n'
+    if size > 1:
+        sql_content += 'UNION\n'
+    size -= 1
+sql_content += ');\n'
+
+# sql_content += 'CREATE TEMPORARY TABLE tmp AS SELECT DISTINCT * FROM customers;\nTRUNCATE customers;\nINSERT INTO customers SELECT * FROM tmp;'
 
 with open('setup.sql', 'w') as table:
     table.write(sql_content)
